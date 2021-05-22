230164aac9640f5529e48a4308956e53409b1247
Joohee Kim <imjhk03@gmail.com>
2021-05-23 02:36:26 +0900

    Change name to LoginViewController

--- SignInWithApple/LoginViewController.swift
+++ SignInWithApple/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  SignInWithApple
 //
 //  Created by Joohee Kim on 21. 05. 23..
@@ -7,11 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemGreen
     }
 
 
